36f3aabcfe68dfb6327dd1ff891cbac8b5dfc860
Jonathan Briggs <jabriggs575@gmail.com>
2026-03-19 09:45:40 -0400

    More work on pdf service.

--- lib/services/pdf_service.dart
+++ lib/services/pdf_service.dart
@@ -53,8 +53,11 @@ class PdfService {
     // Siding
     var sidingSection = _buildSidingSection(measureSheetFromDb);
     // Trim
+    var trimSection = _buildTrimSection(measureSheetFromDb);
     // Door Details
+    var doorDetailsSection = _buildDoorDetailsSection(measureSheetFromDb);
     // Products
+    var productsSection = _buildProductsSection(measureSheetFromDb);
 
     var pageOneContent = pw.Column(
       crossAxisAlignment: pw.CrossAxisAlignment.start,
@@ -74,7 +77,24 @@ class PdfService {
           padding: pw.EdgeInsets.all(8.0),
         ),
         pw.Container(
-          child: pw.Column(children: [homeDetailsSection, sidingSection]),
+          child: pw.Column(children: [
+            homeDetailsSection,
+            pw.SizedBox(height: 2.0),
+            sidingSection,
+            pw.SizedBox(height: 2.0),
+            trimSection,
+            pw.SizedBox(height: 2.0),
+            doorDetailsSection,
+          ]),
+          decoration: pw.BoxDecoration(
+              border: pw.Border.all(color: PdfColors.black),
+              borderRadius: pw.BorderRadius.circular(5)),
+          padding: pw.EdgeInsets.all(8.0),
+        ),
+        pw.Container(
+          child: pw.Column(children: [
+            productsSection,
+          ]),
           decoration: pw.BoxDecoration(
               border: pw.Border.all(color: PdfColors.black),
               borderRadius: pw.BorderRadius.circular(5)),
@@ -91,27 +111,6 @@ class PdfService {
         },
       ),
     );
-
-    //
-    // pdf.addPage(
-    //   pw.Page(
-    //     orientation: pw.PageOrientation.portrait,
-    //     build: (_) {
-    //       return pw.Column(
-    //         children: [
-    //           pw.Padding(
-    //             padding: pw.EdgeInsets.only(top: rowVerticalPadding * 2),
-    //           ),
-    //           _buildHomeDetails(measureSheetFromDb!),
-    //           pw.Padding(
-    //             padding: pw.EdgeInsets.only(top: rowVerticalPadding * 2),
-    //           ),
-    //           _buildProducts(measureSheetFromDb!),
-    //         ],
-    //       );
-    //     },
-    //   ),
-    // );
 
     return pdf;
   }
@@ -272,7 +271,7 @@ class PdfService {
   }
 
   pw.Widget _buildCustomerInformationV2(MeasureSheet measureSheet) {
-    List<pw.Widget> section = [
+    var section = [
       pw.Header(
         level: 1,
         child: pw.Row(
@@ -366,198 +365,6 @@ class PdfService {
     return pw.Column(children: sectionChildren);
   }
 
-  pw.Widget _buildProducts(MeasureSheet measureSheet) {
-    List<pw.Widget> sectionChildren = [
-      pw.Header(
-        level: 1,
-        child: pw.Row(children: [pw.Center(child: pw.Text('Products'))]),
-      ),
-    ];
-
-    sectionChildren.addAll([
-      pw.Padding(padding: pw.EdgeInsets.only(top: rowVerticalPadding)),
-      _buildProductsBoxes(measureSheet.productOptions),
-    ]);
-
-    return pw.Column(children: sectionChildren);
-  }
-
-  pw.Wrap _buildSidingBoxes(SidingOptions sidingOptions) {
-    List<pw.Widget> widgetsToWrap = [];
-
-    widgetsToWrap.add(_buildLabelAndCheckmark('Hardie', sidingOptions.hardie));
-    widgetsToWrap.add(_buildLabelAndCheckmark('Stucco', sidingOptions.stucco));
-    widgetsToWrap.add(_buildLabelAndCheckmark('Vinyl', sidingOptions.vinyl));
-    widgetsToWrap.add(_buildLabelAndCheckmark('Stone', sidingOptions.stone));
-    widgetsToWrap.add(_buildLabelAndCheckmark('Brick', sidingOptions.brick));
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Concrete', sidingOptions.concrete),
-    );
-    widgetsToWrap.add(_buildLabelAndCheckmark('Other', sidingOptions.other));
-
-    if (sidingOptions.other && sidingOptions.otherSpecifics != null) {
-      widgetsToWrap.add(
-        _buildLabelAndValue(
-          'Specifics',
-          sidingOptions.otherSpecifics as String,
-        ),
-      );
-    }
-    return pw.Wrap(spacing: 12.0, runSpacing: 6.0, children: widgetsToWrap);
-  }
-
-  pw.Wrap _buildTrimBoxes(TrimOptions trimOptions) {
-    List<pw.Widget> widgetsToWrap = [];
-
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Top & Bottom', trimOptions.topAndBottom),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('All Around', trimOptions.allAround),
-    );
-    widgetsToWrap.add(_buildLabelAndCheckmark('None', trimOptions.none));
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Keystone', trimOptions.keystone),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Header Stick-out', trimOptions.headerStickout),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Sill Stick-out', trimOptions.sillStickout),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Header Flush', trimOptions.headerFlush),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Sill Flush', trimOptions.sillFlush),
-    );
-
-    if (trimOptions.color != null) {
-      widgetsToWrap.add(
-        _buildLabelAndValue('Color', trimOptions.color as String),
-      );
-    }
-    return pw.Wrap(spacing: 12.0, runSpacing: 6.0, children: widgetsToWrap);
-  }
-
-  pw.Wrap _buildProductsBoxes(ProductOptions productOptions) {
-    List<pw.Widget> widgetsToWrap = [];
-    // if (productOptions.desiredColor != null) {
-    //   widgetsToWrap.add(
-    //     _buildLabelAndValue(
-    //       'Desired Color',
-    //       productOptions.desiredColor as String,
-    //     ),
-    //   );
-    // }
-    // if (productOptions.swNumber != null) {
-    //   widgetsToWrap.add(
-    //     _buildLabelAndValue('S/W #', productOptions.swNumber as String),
-    //   );
-    // }
-    if (productOptions.osb) {
-      widgetsToWrap.add(_buildLabelAndCheckmark('OSB (O)', productOptions.osb));
-    }
-    widgetsToWrap.add(_buildLabelAndCheckmark('Galv (G)', productOptions.galv));
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Alum (Al)', productOptions.alum),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Fabric (F)', productOptions.fabric),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Accordions (Ac)', productOptions.accordions),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Rolldown (R)', productOptions.rolldown),
-    );
-    // todo: updated products from model
-    // widgetsToWrap.add(
-    //   _buildLabelAndCheckmark('Bah (B)/Col (C)', productOptions.bahCol),
-    // );
-    // widgetsToWrap.add(
-    //   _buildLabelAndCheckmark('Lexan (L)', productOptions.lexan),
-    // );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Screen Under', productOptions.screenUnder),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Pool Enclosure', productOptions.poolEnclosure),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Painted Caps', productOptions.paintedCaps),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark(
-        'Bah Articulating (BA)',
-        productOptions.bahArticulating,
-      ),
-    );
-    // widgetsToWrap.add(
-    //   _buildLabelAndCheckmark(
-    //     'Deco Bah (DB)/Deco Col(DC)',
-    //     productOptions.decoBahDecoCol,
-    //   ),
-    // );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Composite', productOptions.composite),
-    );
-    if (productOptions.composite && productOptions.compositeSpecifics != null) {
-      widgetsToWrap.add(
-        _buildLabelAndValue(
-          'Specifics',
-          productOptions.compositeSpecifics as String,
-        ),
-      );
-    }
-    if (productOptions.cutout != null) {
-      widgetsToWrap.add(
-        _buildLabelAndValue('Cutout', productOptions.cutout as String),
-      );
-    }
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Direct Mount (DM)', productOptions.directMount),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Armor Track (AT)', productOptions.armorTrack),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('"H" Header (H)', productOptions.hHeader),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Flat Track (FT)', productOptions.flatTrack),
-    );
-
-    return pw.Wrap(spacing: 12.0, runSpacing: 6.0, children: widgetsToWrap);
-  }
-
-  pw.Wrap _buildDoorDetailsBoxes(DoorDetails doorDetails) {
-    List<pw.Widget> widgetsToWrap = [];
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Room Under Door?', doorDetails.roomUnderDoor),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Concrete Floor', doorDetails.concreteFloor),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark(
-        'Brick/Stone/Tile and Sand Under?',
-        doorDetails.brickStoneTileWithSandUnder,
-      ),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark('Sill in Place?', doorDetails.sillInPlace),
-    );
-    widgetsToWrap.add(
-      _buildLabelAndCheckmark(
-        'Brick/Stone/Tile with Concrete Under?',
-        doorDetails.brickStoneTileWithConcreteUnder,
-      ),
-    );
-
-    return pw.Wrap(spacing: 12.0, runSpacing: 6.0, children: widgetsToWrap);
-  }
-
   String _buildAddress(MeasureSheet measureSheet) {
     var streetNumber = measureSheet.streetNumber;
     var streetName = measureSheet.streetName;
@@ -567,9 +374,7 @@ class PdfService {
     var state = measureSheet.state ?? "";
     var zipCode = measureSheet.zipCode ?? "";
 
-    return
-      """$streetNumber $streetName $lotNumber $plantation\n$cityTown $state, $zipCode
-    """;
+    return '$streetNumber $streetName $lotNumber $plantation\n$cityTown $state, $zipCode';
   }
 
   pw.Widget _buildSidingSection(MeasureSheet measureSheet) {
@@ -592,15 +397,177 @@ class PdfService {
     );
   }
 
+  pw.Widget _buildTrimSection(MeasureSheet measureSheet) {
+    var section = [
+      pw.Header(
+          level: 2,
+          child: pw.Row(children: [
+            pw.Text('Trim',
+                style: pw.TextStyle(decoration: pw.TextDecoration.underline))
+          ])
+      ),
+      pw.Row(
+          children: [
+            pw.Expanded(child: _buildTrimRowContent(measureSheet.trimOptions)),
+          ]
+      ),
+    ];
+    return pw.Column(
+        children: section
+    );
+  }
+
+  pw.Widget _buildDoorDetailsSection(MeasureSheet measureSheet) {
+    var section = [
+      pw.Header(
+          level: 2,
+          child: pw.Row(children: [
+            pw.Text('Door Details',
+                style: pw.TextStyle(decoration: pw.TextDecoration.underline))
+          ])
+      ),
+      pw.Row(
+          children: [
+            pw.Expanded(
+                child: _buildDoorDetailsContent(measureSheet.doorDetails)),
+          ]
+      ),
+    ];
+    return pw.Column(
+        children: section
+    );
+  }
+
+  pw.Widget _buildProductsSection(MeasureSheet measureSheet) {
+    var section = [
+      pw.Header(
+          level: 1,
+          child: pw.Row(children: [
+            pw.Text('Products')
+          ])
+      ),
+      pw.Row(
+          children: [
+            pw.Expanded(
+                child: _buildProductsContent(measureSheet.productOptions)),
+          ]
+      ),
+    ];
+
+    return pw.Column(
+        children: section
+    );
+  }
+
   pw.Widget _buildSidingRowContent(SidingOptions sidingOptions) {
     String content = '';
     var sidingMap = sidingOptions.toMap();
     content = sidingMap.entries
-        .where((entry) => entry.value != false)
+        .where((entry) =>
+    entry.value != false &&
+        !['other', 'color', 'otherSpecifics'].contains(entry.key))
         .map((entry) => entry.key)
         .join(", ");
+
+    // add other and color if necessary
+    if (sidingMap['other']) {
+      var prefixComma = !content.endsWith(',');
+      if (prefixComma) {
+        content += ', ';
+      }
+      content += 'other: ${sidingMap['otherSpecifics']}';
+    }
+    if ((sidingMap['color'] as String).isNotEmpty) {
+      var prefixComma = !content.endsWith(',');
+      if (prefixComma) {
+        content += ', ';
+      }
+      content += 'color: ${sidingMap['color']}';
+    }
+
     return pw.Text(content);
   }
+
+  pw.Widget _buildTrimRowContent(TrimOptions trimOptions) {
+    String content = '';
+    var trimMap = trimOptions.toMap();
+    content = trimMap.entries
+        .where((entry) =>
+    entry.value != false && !['color'].contains(entry.key))
+        .map((entry) => entry.key)
+        .join(", ");
+
+    if ((trimMap['color'] as String).isNotEmpty) {
+      var prefixComma = !content.endsWith(',');
+      if (prefixComma) {
+        content += ', ';
+      }
+      content += 'color: ${trimMap['color']}';
+    }
+
+    return pw.Text(content, softWrap: true);
+  }
+
+  pw.Widget _buildDoorDetailsContent(DoorDetails doorDetails) {
+    String content = '';
+    var ddMap = doorDetails.toMap();
+    content = ddMap.entries
+        .where((entry) => entry.value)
+        .map((entry) => entry.key)
+        .join(", ");
+
+    return pw.Text(content, softWrap: true);
+  }
+
+  pw.Widget _buildProductsContent(ProductOptions productOptions) {
+    String content = '';
+    var productsMap = productOptions.toMap();
+    content = productsMap.entries
+        .where((entry) =>
+    entry.value != null && entry.value != false && ![
+      'paintBrand',
+      'otherBrandSpecify',
+      'paintCode',
+      'composite',
+      'compositeSpecifics',
+      'cutout',
+      'Null'
+    ].contains(entry.key))
+        .map((entry) => entry.key)
+        .join(", ");
+
+    if (productsMap['paintBrand'] == 'Other') {
+      var prefixComma = !content.endsWith(',');
+      if (prefixComma) {
+        content += ', ';
+      }
+      content += 'paint brand: ${productsMap['otherBrandSpecify']}';
+    }
+    if ((productsMap['paintCode'] as String).isNotEmpty) {
+      var prefixComma = !content.endsWith(',');
+      if (prefixComma) {
+        content += ', ';
+      }
+      content += 'paint code: ${productsMap['paintCode']}';
+    }
+    if (productsMap['composite']) {
+      var prefixComma = !content.endsWith(',');
+      if (prefixComma) {
+        content += ', ';
+      }
+      content += 'composite: ${productsMap['compositeSpecifics']}';
+    }
+    if (productsMap['cutout'] != null &&
+        (productsMap['cutout'] as String).isNotEmpty) {
+      var prefixComma = !content.endsWith(',');
+      if (prefixComma) {
+        content += ', ';
+      }
+      content += 'cutout: ${productsMap['cutout']}';
+    }
+    return pw.Text(content, softWrap: true);
+  }
+
 }
 
 class MeasureSheetNotFoundException implements Exception {
